577755610de8665abd0c86219a61ed0bd9fa18d3
Nat <98453322+nat-sorokina@users.noreply.github.com>
2023-03-24 14:22:16 +0000

    Update script.js
    update file so that it works in vs code console

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-let humanScore = 0;
-let computerScore = 0;
-let currentRoundNumber = 1;
+//Type in any integer between 1 and 10 and play against the computer - see whose number is closer to a secret target
 
-// Write your code below:
-const generateTarget = () => Math.floor(Math.random()*10);
+const humanGuess = 7; //Type any integer between 1 and 10 here;
+console.log(`Your chosen number is: ${humanGuess}`);
 
-const compareGuesses = (humanGuess, computerGuess, secretTarget) => {
+const secretTarget = Math.floor(Math.random()*10)+1;
+console.log(`The secret target is: ${secretTarget}`);
+
+const computerGuess = Math.floor(Math.random()*10)+1;
+console.log(`Computer's chosen number is: ${computerGuess}`);
+
+
+const compareGuesses = () => {
 const valueHuman = Math.abs(secretTarget - humanGuess);
 const valueComputer = Math.abs(secretTarget - computerGuess);
- if (valueHuman <= valueComputer) {
-  return true;
+ if (valueHuman<=valueComputer || valueHuman === secretTarget) {
+  return 'You won!';
  }
-else {
-  return false;
+ else {
+  return `Computer won!`;
 }
-}
-compareGuesses();
-
-const updateScore = winnerIs => {
-  if (winnerIs === 'human') {
-   humanScore += 1;
-  }
-  else if (winnerIs === 'computer') {
-    computerScore += 1;
-  }
 }
 
-const advanceRound = () => {
-  currentRoundNumber += 1;
-}
+console.log(compareGuesses());
 
 
 
